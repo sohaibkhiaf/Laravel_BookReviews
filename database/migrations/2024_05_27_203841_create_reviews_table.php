ff01b53f7efbb217c4a4f71075c7e61a61dc5d12
Khiaf Sohaib <sohaibkhiaf@gmail.com>
2024-05-27 22:29:32 +0100

    first commit

---
+++ database/migrations/2024_05_27_203841_create_reviews_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('review');
+            $table->unsignedTinyInteger('rating');
+            $table->unsignedBigInteger('book_id');
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
